3b46103535ee8249f8301016bcad5292cf025e42
qingmingzi <matrix3345@hotmail.com>
2018-12-07 13:28:37 +0800

    init

---
+++ NEO_Metadata_Check/NEL_Mongo_Helper/NEL_MongoHelper.cs
@@ -0,0 +1,173 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.IO;
+using MongoDB.Driver;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NEL.MongoHelper
+{
+    public class NEL_MongoHelper
+    {
+        string mongodbConnStr;
+        string mongodbDatabase;
+
+        public NEL_MongoHelper(string mongodbConnStrIn, string mongodbDatabaseIn) {
+            mongodbConnStr = mongodbConnStrIn;
+            mongodbDatabase = mongodbDatabaseIn;
+        }
+
+        public JArray GetData(string coll, string findFliter, string sortFliter = "{}", int limit = 0)
+        {
+            var client = new MongoClient(mongodbConnStr);
+            var database = client.GetDatabase(mongodbDatabase);
+            var collection = database.GetCollection<BsonDocument>(coll);
+
+            List<BsonDocument> query = new List<BsonDocument>();
+            if (limit == 0)
+            {
+                query = collection.Find(BsonDocument.Parse(findFliter)).Sort(sortFliter).ToList();
+            }
+            else
+            {
+                query = collection.Find(BsonDocument.Parse(findFliter)).Sort(sortFliter).Limit(limit).ToList();
+            }
+            client = null;
+
+            if (query.Count > 0)
+            {
+                var jsonWriterSettings = new JsonWriterSettings { OutputMode = JsonOutputMode.Strict };
+                JArray JA = JArray.Parse(query.ToJson(jsonWriterSettings));
+                foreach (JObject j in JA)
+                {
+                    j.Remove("_id");
+                }
+                return JA;
+            }
+            else { return new JArray(); }
+        }
+
+        //批量写入数据
+        public void InsertDataByJarray(string coll, JArray Jdata)
+        {
+            var client = new MongoClient(mongodbConnStr);
+            var database = client.GetDatabase(mongodbDatabase);
+            var collection = database.GetCollection<BsonDocument>(coll);
+
+            List<BsonDocument> bsons = new List<BsonDocument>();
+            foreach (JObject J in Jdata)
+            {
+                string strData = Newtonsoft.Json.JsonConvert.SerializeObject(J);
+                BsonDocument bson = BsonDocument.Parse(strData);
+                bsons.Add(bson);
+            }
+
+            //collection.InsertOne(bsons[0]);
+            collection.InsertMany(bsons.ToArray());
+
+            client = null;
+        }
+
+        // 单个入库
+        public void InsertData(string coll, JObject Jdata)
+        {
+            var client = new MongoClient(mongodbConnStr);
+            var database = client.GetDatabase(mongodbDatabase);
+            var collection = database.GetCollection<BsonDocument>(coll);
+
+            string strData = Newtonsoft.Json.JsonConvert.SerializeObject(Jdata);
+            BsonDocument bson = BsonDocument.Parse(strData);
+            collection.InsertOne(bson);
+
+            client = null;
+        }
+
+        public long GetDataCount(string coll, JObject Jdata = null)
+        {
+            return GetDataCount(coll, Jdata == null ? "{}" : Jdata.ToString());
+        }
+        public long GetDataCount(string coll, string findBson)
+        {
+            var client = new MongoClient(mongodbConnStr);
+            var database = client.GetDatabase(mongodbDatabase);
+            var collection = database.GetCollection<BsonDocument>(coll);
+
+            var txCount = collection.Find(BsonDocument.Parse(findBson)).Count();
+
+            client = null;
+
+            return txCount;
+        }
+
+        public int GetSystemCounter(string counter)
+        {
+            int maxIndex = -1;
+            var client = new MongoClient(mongodbConnStr);
+            var database = client.GetDatabase(mongodbDatabase);
+            var collection = database.GetCollection<BsonDocument>("system_counter");
+
+            var queryBson = BsonDocument.Parse("{counter:'" + counter + "'}");
+            var query = collection.Find(queryBson).ToList();
+            if (query.Count == 0) { maxIndex = -1; }
+            else
+            {
+                maxIndex = (int)query[0]["lastBlockindex"];
+            }
+
+            client = null;
+            return maxIndex;
+        }
+
+        public void SetSystemCounter(string counter, Int64 lastBlockindex)
+        {
+            var client = new MongoClient(mongodbConnStr);
+            var database = client.GetDatabase(mongodbDatabase);
+            var collection = database.GetCollection<BsonDocument>("system_counter");
+
+            var setBson = BsonDocument.Parse("{counter:'" + counter + "',lastBlockindex:" + lastBlockindex + "}");
+
+            var queryBson = BsonDocument.Parse("{counter:'" + counter + "'}");
+            var query = collection.Find(queryBson).ToList();
+            if (query.Count == 0)
+            {
+                collection.InsertOne(setBson);
+            }
+            else
+            {
+                collection.ReplaceOne(queryBson, setBson);
+            }
+
+            client = null;
+        }
+
+        public void setIndex(string coll, string indexDefinition, string indexName, bool isUnique = false)
+        {
+            var client = new MongoClient(mongodbConnStr);
+            var database = client.GetDatabase(mongodbDatabase);
+            var collection = database.GetCollection<BsonDocument>(coll);
+
+            //检查是否已有设置idnex
+            bool isSet = false;
+            using (var cursor = collection.Indexes.List())
+            {
+                JArray JAindexs = JArray.Parse(cursor.ToList().ToJson());
+                var query = JAindexs.Children().Where(index => (string)index["name"] == indexName);
+                if (query.Count() > 0) isSet = true;
+                // do something with the list...
+            }
+
+            if (!isSet)
+            {
+                try
+                {
+                    var options = new CreateIndexOptions { Name = indexName, Unique = isUnique };
+                    collection.Indexes.CreateOne(indexDefinition, options);
+                }
+                catch { }
+            }
+
+            client = null;
+        }
+    }
+}
---
+++ NEO_Metadata_Check/NEO_Metadata_Check/Program.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using NEL.MongoHelper ;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+using System.Linq;
+
+namespace NEO_Metadata_Check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddInMemoryCollection()    //将配置文件的数据加载到内存中
+                .SetBasePath(Directory.GetCurrentDirectory())   //指定配置文件所在的目录
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)  //指定加载的配置文件
+                .Build();    //编译成对象
+
+            string mongodbConnStr_data = config["mongodbConnStr_data"];
+            string mongodbDatabase_data = config["mongodbDatabase_data"];
+            string mongodbConnStr_target = config["mongodbConnStr_target"];
+            string mongodbDatabase_target = config["mongodbDatabase_target"];
+
+            NEL_MongoHelper nEL_MongoHelper_data = new NEL_MongoHelper(mongodbConnStr_data, mongodbDatabase_data);
+            NEL_MongoHelper nEL_MongoHelper_target = new NEL_MongoHelper(mongodbConnStr_target, mongodbDatabase_target);
+
+            Console.WriteLine("input blockHeight_target start:");
+            Console.WriteLine("1 : check Blcok");
+            Console.WriteLine("2 : check Tx");
+            Int64 modeCode = Int64.Parse(Console.ReadLine());
+
+            Console.WriteLine("input blockHeight_target start:");
+            Int64 blockHeight_target_start = Int64.Parse(Console.ReadLine());
+
+            Console.WriteLine("input blockHeight_target end:");
+            Int64 blockHeight_target_end = Int64.Parse(Console.ReadLine());
+
+            //初始化
+            //nEL_MongoHelper_data.SetSystemCounter("block_check", -1);
+
+            Console.WriteLine("start......");
+            for (Int64 doBlockIndex= blockHeight_target_start; doBlockIndex <= blockHeight_target_end; doBlockIndex++)
+            {
+                JArray block = nEL_MongoHelper_target.GetData("block", "{index:" + doBlockIndex + "}");
+
+                switch (modeCode) {
+                    case 1:
+                        if (block.Count != 1)
+                        {
+                            nEL_MongoHelper_data.InsertData("block_error", JObject.Parse("{index:" + doBlockIndex + "}"));
+                        }
+                        nEL_MongoHelper_data.SetSystemCounter("block_check", doBlockIndex);
+                        break;
+                    case 2:
+                        JArray TXs_Source = (JArray)block[0]["tx"];
+                        
+                        List<string> TXIDs_Source = new List<string>();
+                        foreach (JObject J in TXs_Source)
+                        {
+                            TXIDs_Source.Add((string)J["txid"]);
+                        }
+
+                        JArray TXs_Target = nEL_MongoHelper_target.GetData("tx", "{blockindex:" + doBlockIndex + "}");
+                        List<string> TXIDs_Target = new List<string>();
+                        foreach (JObject J in TXs_Target)
+                        {
+                            TXIDs_Target.Add((string)J["txid"]);
+                        }
+
+                        //少入的txid
+                        var TXIDs_Except_Source_Target = TXIDs_Source.Except(TXIDs_Target);
+                        if (TXIDs_Except_Source_Target.Count() > 0)
+                        {
+                            foreach (string txid in TXIDs_Except_Source_Target)
+                            {
+                                nEL_MongoHelper_data.InsertData("txid_error_less", JObject.Parse("{index:" + doBlockIndex + ",txid:'" + txid +"'}"));
+                            }
+                            
+                        }
+
+                        //多入的txid
+                        var TXIDs_Except_Target_Source = TXIDs_Target.Except(TXIDs_Source);
+                        if (TXIDs_Except_Target_Source.Count() > 0)
+                        {
+                            foreach (string txid in TXIDs_Except_Target_Source)
+                            {
+                                nEL_MongoHelper_data.InsertData("txid_error_many", JObject.Parse("{index:" + doBlockIndex + ",txid:'" + txid + "'}"));
+                            }
+                        }
+
+                        nEL_MongoHelper_data.SetSystemCounter("tx_check", doBlockIndex);
+                        break;
+                    default:
+                        break;
+                }
+
+                if ((doBlockIndex % 1000) == 0)
+                {
+                    Console.WriteLine(((decimal)(doBlockIndex - blockHeight_target_start) / (decimal)(blockHeight_target_end - blockHeight_target_start)).ToString("p4"));
+                }
+
+                //Thread.Sleep(5);
+            }
+
+        }
+    }
+}
